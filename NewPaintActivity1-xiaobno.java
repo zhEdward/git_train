ae53a474b17445527f64225ef9a3c6ed3764b031
Edward <1514505790@qq.com>
2016-08-27 12:38:21 +0800

    NewPaintActivity1-xiaobno.java in branch iss53 delete some import package

--- NewPaintActivity1-xiaobno.java
+++ NewPaintActivity1-xiaobno.java
@@ -1,26 +1,5 @@
 package com.android.androidpaint;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.cocos2dx.cpp.AppActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
